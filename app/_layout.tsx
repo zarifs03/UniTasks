d45774de8334da05301b0e2b40af36cb5fa9de12
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-07-03 00:26:16 +0800

    initial header done along with placeholder welcome message.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,61 @@
-import { Text, View } from "react-native";
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function DashboardScreen() {
+  // today's date
+  const today = new Date();
+
+  const formattedDate = today.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>UniTasks</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome back, Username!</Text>
+        <Text style={styles.todayText}>{formattedDate}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+  },
+  header: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    paddingTop: Platform.OS === 'android' ? (StatusBar.currentHeight ?? 0) + 8 : 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 14,
+    backgroundColor: '#FAFAFA',
+  },
+  logo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#4A00E0',
+  },
+  welcome: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 6,
+  },
+  todayText: {
+    fontSize: 12,
+    color: '#333',
+  },
+});
