1e2b29222cb92e5510008b4d893d6ae557074d91
Zarif-Solaiman <23640056@student.uwa.edu.au>
2025-07-08 19:51:15 +0800

    Android navigation buttons now dark

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,15 @@ import * as NavigationBar from 'expo-navigation-bar';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
 import { ProjectProvider } from '../context/ProjectContext';
 
 export default function Layout() {
   useEffect(() => {
-    // Set navigation bar style for Android
-    if (Platform.OS === 'android') {
-      // Set navigation bar to use dark content (dark buttons) using expo-navigation-bar
-      NavigationBar.setButtonStyleAsync('dark');
-      
-      console.log('Navigation bar set to use dark buttons');
-    }
+    const setNavBar = async () => {
+      await NavigationBar.setVisibilityAsync('visible');
+      await NavigationBar.setButtonStyleAsync('dark');
+    };
+    setNavBar();
   }, []);
 
   return (
